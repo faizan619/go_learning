52af54329af68a253b192f6c217280b9a05ea06b
faizan alam <alamf6023@gmail.com>
2025-02-26 14:45:29 +0530

    initialize and start the go project

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Learn Go Language in this.")
+	fmt.Println("===============================")
+	fmt.Print("Faizan ")
+	fmt.Print("Alam")
+}

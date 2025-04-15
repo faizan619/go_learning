fa5bc9994d67474e0fc37b697d7c45fb3b820726
Faizan Alam <95950378+faizan619@users.noreply.github.com>
2025-04-16 00:16:23 +0530

    learning from the book

--- main.go
+++ main.go
@@ -8,3 +8,39 @@ func main(){
 	fmt.Print("Faizan ")
 	fmt.Print("Alam")
 }
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
+
+var name = "faizan"
+
+// sirname := "alam"	// it gives error
+
+func main() {
+	var sent = "my name is faizan \nI am graduated student from mumbai \nI am Single"
+	fmt.Println(sent)
+	fmt.Println(reflect.TypeOf(sent))
+	var quate = `"never trust anyone \nI repeat never"
+			got hated one.
+			But not twice
+			- faizan `
+	fmt.Println(quate)
+	fmt.Printf("%T\n", quate)
+	// var unused = "not using this variable"
+	// _ = unused	// now it will not give not used error
+	// fmt.Println(name)
+	//fmt.Println(sirname)
+	// var num float64
+	// var str string
+	// str1 := "hello world"
+	// fmt.Println(str1)
+	// fmt.Println(num)
+	// fmt.Println(str)
+	// fmt.Println("Hello! Welcome to the World")
+	fmt.Println("Current Time : ", time.Now())
+}
